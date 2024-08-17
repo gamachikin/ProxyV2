48238e74783a7eae83e478df2aa8b6a3af2d0963
gamachikin <102493636+gamachikin@users.noreply.github.com>
2024-08-17 12:46:41 -0400

    e

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { ChemicalServer } from "chemicaljs";
 import express from "express";
-import { execSync } from "node:child_process";
-import fs from "node:fs";
+import { execSync } from "child_process";
+import fs from "fs";
 
 if (!fs.existsSync("dist")) {
     console.log("No build folder found. Building...");
@@ -13,11 +13,9 @@ const chemical = new ChemicalServer({
     scramjet: false,
     rammerhead: false,
 });
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the "dist" directory
 app.use(
     express.static("dist", {
         index: "index.html",
@@ -25,16 +23,13 @@ app.use(
     })
 );
 
-// Handle 404 errors with a custom error page
 app.use((req, res) => {
     res.status(404);
     res.sendFile("dist/index.html", { root: "." });
 });
 
-// Use the ChemicalServer middleware
 app.use(chemical.middleware());
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
