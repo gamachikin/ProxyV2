812962933969488f2d5f51c67e35aa2d99213690
gamachikin <102493636+gamachikin@users.noreply.github.com>
2024-08-17 13:32:43 -0400

    e

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import { ChemicalServer } from "chemicaljs";
 import express from "express";
 
-const chemical = new ChemicalServer({
-    rammerhead: false,
-});
+const chemical = new ChemicalServer();
 const port = process.env.PORT || 3000;
 
 chemical.use(express.static("public", {
@@ -11,6 +9,11 @@ chemical.use(express.static("public", {
     extensions: ["html"]
 }));
 
+chemical.error((req, res) => {
+    res.status(404);
+    res.send("404 Error");
+});
+
 chemical.listen(port, () => {
-    console.log(`Chemical example build listening on port ${port}`);
+    console.log(`Chemical example listening on port ${port}`);
 });
